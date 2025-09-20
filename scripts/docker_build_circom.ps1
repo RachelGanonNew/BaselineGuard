22c4e49e5545086ecb92768796ad8e524696459b
RachelGanonNew <r05458410430@gmail.com>
2025-09-20 23:57:13 +0300

    ci: replace ekzhang/circom with iden3/circom across scripts and workflows

--- scripts/docker_build_circom.ps1
+++ scripts/docker_build_circom.ps1
@@ -4,5 +4,5 @@ Param(
 
 Write-Host "Using Docker to compile $InputCircuit"
 $pwd = (Get-Location).Path
-docker run --rm -v "${pwd}:/work" -w /work ekzhang/circom:latest circom $InputCircuit --r1cs --wasm --sym -o circuits/build
+docker run --rm -v "${pwd}:/work" -w /work iden3/circom:latest circom $InputCircuit --r1cs --wasm --sym -o circuits/build
 Write-Host "Done. Artifacts in circuits/build"
